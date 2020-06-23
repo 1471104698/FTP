c52599a792d92fec4fd0f99d7bcbdb15c4242c38
1471104698 <48862368+1471104698@users.noreply.github.com>
2020-06-23 18:43:37 +0800

    第三次重构，优化 cd 功能，简化路径

--- src/cn/oy/test/processor/impl/CdProcessor.java
+++ src/cn/oy/test/processor/impl/CdProcessor.java
@@ -5,6 +5,10 @@ import cn.oy.test.io.FTPServer;
 import cn.oy.test.model.Order;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author 蒜头王八
@@ -16,7 +20,7 @@ public class CdProcessor implements Commond {
     @Override
     public void commond(Order order, FTPServer ftpServer) {
         //获取 cd 的目录
-        String path = order.getMsg();
+        String path = simplifyPath(order.getMsg());
         if(path.startsWith("\\") || path.startsWith("/")){
             path = ftpServer.getPath() + path + File.separator;
         }else{
@@ -24,5 +28,39 @@ public class CdProcessor implements Commond {
         }
         ftpServer.sendLineUTF("目录切换成功");
         ftpServer.setCur_path(path);
+    }
+
+    /**
+     * 简化路径
+     * @param path
+     * @return
+     */
+    public String simplifyPath(String path) {
+        String[] paths = path.split("/");
+        Deque<String> stack = new LinkedList<>();
+        for(String pa : paths){
+            if(".".equals(pa) || "".equals(pa)){
+                continue;
+            }
+            if("..".equals(pa)){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+                continue;
+            }
+            stack.push(pa);
+        }
+        if(stack.isEmpty()){
+            return "/";
+        }
+        List<String> res = new ArrayList<>();
+        while(!stack.isEmpty()){
+            res.add(stack.pop());
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = res.size() - 1; i >= 0; i--){
+            sb.append("/").append(res.get(i));
+        }
+        return sb.toString();
     }
 }
